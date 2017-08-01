5b9c6f6443823b27083de3eacdb4dc1b1a032e39
Oclemy <oclemmi@gmail.com>
2017-08-01 15:07:08 +0300

    Initial Commit

---
+++ VB_ListView_Access/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿Imports MetroFramework.Forms
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mListView = New System.Windows.Forms.ListView()
+        Me.nameTxt = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.propellantTxt = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.destinationTxt = New MetroFramework.Controls.MetroTextBox()
+        Me.addBtn = New MetroFramework.Controls.MetroButton()
+        Me.retrieveBtn = New MetroFramework.Controls.MetroButton()
+        Me.updateBtn = New MetroFramework.Controls.MetroButton()
+        Me.deleteBtn = New MetroFramework.Controls.MetroButton()
+        Me.clearBtn = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'mListView
+        '
+        Me.mListView.Location = New System.Drawing.Point(24, 106)
+        Me.mListView.Name = "mListView"
+        Me.mListView.Size = New System.Drawing.Size(557, 349)
+        Me.mListView.TabIndex = 0
+        Me.mListView.UseCompatibleStateImageBehavior = False
+        '
+        'nameTxt
+        '
+        '
+        '
+        '
+        Me.nameTxt.CustomButton.Image = Nothing
+        Me.nameTxt.CustomButton.Location = New System.Drawing.Point(125, 1)
+        Me.nameTxt.CustomButton.Name = ""
+        Me.nameTxt.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.nameTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.nameTxt.CustomButton.TabIndex = 1
+        Me.nameTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.nameTxt.CustomButton.UseSelectable = True
+        Me.nameTxt.CustomButton.Visible = False
+        Me.nameTxt.Lines = New String(-1) {}
+        Me.nameTxt.Location = New System.Drawing.Point(731, 116)
+        Me.nameTxt.MaxLength = 32767
+        Me.nameTxt.Name = "nameTxt"
+        Me.nameTxt.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.nameTxt.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.nameTxt.SelectedText = ""
+        Me.nameTxt.SelectionLength = 0
+        Me.nameTxt.SelectionStart = 0
+        Me.nameTxt.ShortcutsEnabled = True
+        Me.nameTxt.Size = New System.Drawing.Size(147, 23)
+        Me.nameTxt.TabIndex = 1
+        Me.nameTxt.UseSelectable = True
+        Me.nameTxt.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.nameTxt.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'MetroLabel1
+        '
+        Me.MetroLabel1.AutoSize = True
+        Me.MetroLabel1.Location = New System.Drawing.Point(620, 120)
+        Me.MetroLabel1.Name = "MetroLabel1"
+        Me.MetroLabel1.Size = New System.Drawing.Size(45, 19)
+        Me.MetroLabel1.TabIndex = 2
+        Me.MetroLabel1.Text = "Name"
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.Location = New System.Drawing.Point(620, 174)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(69, 19)
+        Me.MetroLabel2.TabIndex = 4
+        Me.MetroLabel2.Text = "Propellant"
+        '
+        'propellantTxt
+        '
+        '
+        '
+        '
+        Me.propellantTxt.CustomButton.Image = Nothing
+        Me.propellantTxt.CustomButton.Location = New System.Drawing.Point(125, 1)
+        Me.propellantTxt.CustomButton.Name = ""
+        Me.propellantTxt.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.propellantTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.propellantTxt.CustomButton.TabIndex = 1
+        Me.propellantTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.propellantTxt.CustomButton.UseSelectable = True
+        Me.propellantTxt.CustomButton.Visible = False
+        Me.propellantTxt.Lines = New String(-1) {}
+        Me.propellantTxt.Location = New System.Drawing.Point(731, 170)
+        Me.propellantTxt.MaxLength = 32767
+        Me.propellantTxt.Name = "propellantTxt"
+        Me.propellantTxt.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.propellantTxt.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.propellantTxt.SelectedText = ""
+        Me.propellantTxt.SelectionLength = 0
+        Me.propellantTxt.SelectionStart = 0
+        Me.propellantTxt.ShortcutsEnabled = True
+        Me.propellantTxt.Size = New System.Drawing.Size(147, 23)
+        Me.propellantTxt.TabIndex = 3
+        Me.propellantTxt.UseSelectable = True
+        Me.propellantTxt.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.propellantTxt.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'MetroLabel3
+        '
+        Me.MetroLabel3.AutoSize = True
+        Me.MetroLabel3.Location = New System.Drawing.Point(620, 239)
+        Me.MetroLabel3.Name = "MetroLabel3"
+        Me.MetroLabel3.Size = New System.Drawing.Size(73, 19)
+        Me.MetroLabel3.TabIndex = 6
+        Me.MetroLabel3.Text = "Destination"
+        '
+        'destinationTxt
+        '
+        '
+        '
+        '
+        Me.destinationTxt.CustomButton.Image = Nothing
+        Me.destinationTxt.CustomButton.Location = New System.Drawing.Point(125, 1)
+        Me.destinationTxt.CustomButton.Name = ""
+        Me.destinationTxt.CustomButton.Size = New System.Drawing.Size(21, 21)
+        Me.destinationTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.destinationTxt.CustomButton.TabIndex = 1
+        Me.destinationTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.destinationTxt.CustomButton.UseSelectable = True
+        Me.destinationTxt.CustomButton.Visible = False
+        Me.destinationTxt.Lines = New String(-1) {}
+        Me.destinationTxt.Location = New System.Drawing.Point(731, 235)
+        Me.destinationTxt.MaxLength = 32767
+        Me.destinationTxt.Name = "destinationTxt"
+        Me.destinationTxt.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.destinationTxt.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.destinationTxt.SelectedText = ""
+        Me.destinationTxt.SelectionLength = 0
+        Me.destinationTxt.SelectionStart = 0
+        Me.destinationTxt.ShortcutsEnabled = True
+        Me.destinationTxt.Size = New System.Drawing.Size(147, 23)
+        Me.destinationTxt.TabIndex = 5
+        Me.destinationTxt.UseSelectable = True
+        Me.destinationTxt.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.destinationTxt.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel)
+        '
+        'addBtn
+        '
+        Me.addBtn.Location = New System.Drawing.Point(620, 314)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(111, 23)
+        Me.addBtn.TabIndex = 7
+        Me.addBtn.Text = "Add"
+        Me.addBtn.UseSelectable = True
+        '
+        'retrieveBtn
+        '
+        Me.retrieveBtn.Location = New System.Drawing.Point(764, 314)
+        Me.retrieveBtn.Name = "retrieveBtn"
+        Me.retrieveBtn.Size = New System.Drawing.Size(114, 23)
+        Me.retrieveBtn.TabIndex = 8
+        Me.retrieveBtn.Text = "Retrieve"
+        Me.retrieveBtn.UseSelectable = True
+        '
+        'updateBtn
+        '
+        Me.updateBtn.Location = New System.Drawing.Point(620, 369)
+        Me.updateBtn.Name = "updateBtn"
+        Me.updateBtn.Size = New System.Drawing.Size(111, 23)
+        Me.updateBtn.TabIndex = 9
+        Me.updateBtn.Text = "Update"
+        Me.updateBtn.UseSelectable = True
+        '
+        'deleteBtn
+        '
+        Me.deleteBtn.Location = New System.Drawing.Point(764, 369)
+        Me.deleteBtn.Name = "deleteBtn"
+        Me.deleteBtn.Size = New System.Drawing.Size(114, 23)
+        Me.deleteBtn.TabIndex = 10
+        Me.deleteBtn.Text = "Delete"
+        Me.deleteBtn.UseSelectable = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(688, 420)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(111, 23)
+        Me.clearBtn.TabIndex = 11
+        Me.clearBtn.Text = "Clear"
+        Me.clearBtn.UseSelectable = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(930, 532)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.deleteBtn)
+        Me.Controls.Add(Me.updateBtn)
+        Me.Controls.Add(Me.retrieveBtn)
+        Me.Controls.Add(Me.addBtn)
+        Me.Controls.Add(Me.MetroLabel3)
+        Me.Controls.Add(Me.destinationTxt)
+        Me.Controls.Add(Me.MetroLabel2)
+        Me.Controls.Add(Me.propellantTxt)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Controls.Add(Me.nameTxt)
+        Me.Controls.Add(Me.mListView)
+        Me.Name = "Form1"
+        Me.Text = "VB.NET ListView MS Access CRUD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents mListView As System.Windows.Forms.ListView
+    Friend WithEvents nameTxt As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents propellantTxt As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents destinationTxt As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents addBtn As MetroFramework.Controls.MetroButton
+    Friend WithEvents retrieveBtn As MetroFramework.Controls.MetroButton
+    Friend WithEvents updateBtn As MetroFramework.Controls.MetroButton
+    Friend WithEvents deleteBtn As MetroFramework.Controls.MetroButton
+    Friend WithEvents clearBtn As MetroFramework.Controls.MetroButton
+
+End Class
---
+++ VB_ListView_Access/Form1.vb
@@ -0,0 +1,229 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+
+    'DECLARATIONS
+    Private Const conString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:/Users/Hp/Documents/DataBases/spacecraftsDB.mdb;"
+    ReadOnly con As OleDbConnection = New OleDbConnection(conString)
+    Dim cmd As OleDbCommand
+    Dim adapter As OleDbDataAdapter
+    ReadOnly dt As DataTable = New DataTable()
+
+    'WHEN FORM IS LOADED
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.SetupListView()
+    End Sub
+
+    'SETUP LISTVIEW
+    Private Sub SetupListView()
+        'SET LISTVIEW PROPERTIES
+        mListView.View = View.Details
+        mListView.FullRowSelect = True
+
+        'CONSTRUCT COLUMNS
+        mListView.Columns.Add("ID", 50)
+        mListView.Columns.Add("Name", 160)
+        mListView.Columns.Add("Propellant", 170)
+        mListView.Columns.Add("Destination", 170)
+    End Sub
+
+    'INSERT INTO DATABASE
+    Public Sub Add()
+
+        'INSERT SQL STATEMENT
+        Const SQL As String = "INSERT INTO spacecraftsTB(S_Name,S_Propellant,S_Destination) VALUES(@NAME,@PROPELLANT,@DESTINATION)"
+        cmd = New OleDbCommand(SQL, con)
+
+        'ADD PARAMETERS
+        cmd.Parameters.AddWithValue("@NAME", nameTxt.Text)
+        cmd.Parameters.AddWithValue("@PROPELLANT", propellantTxt.Text)
+        cmd.Parameters.AddWithValue("@DESTINATION", destinationTxt.Text)
+
+        'OPEN CONNECTION And INSERT INTO DATABASE THEN CLOSE CONNECTION
+        Try
+            con.Open()
+            If cmd.ExecuteNonQuery() > 0 Then
+                MsgBox("Successfully Inserted")
+                CleartextBoxes()
+            End If
+            con.Close()
+            Retrieve()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            con.Close()
+        End Try
+    End Sub
+
+    'POPULATE LISTVIEW
+    Private Sub Populate(id As String, spacecraft As String, propellant As String, destination As String)
+
+        'ROW ARRAY
+        Dim row As String() = New String() {id, spacecraft, propellant, destination}
+        Dim item As ListViewItem = New ListViewItem(row)
+
+        'ADD TO ROWS COLLECTION
+        mListView.Items.Add(item)
+    End Sub
+
+    'RETRIEVE FROM DataBase
+    Private Sub Retrieve()
+
+        mListView.Items.Clear()
+
+        'SELECT SQL STATEMENT
+        Dim sql As String = "SELECT * FROM spacecraftsTB "
+        cmd = New OleDbCommand(sql, con)
+
+        'OPEN CONNECTION,RETRIEVE,FILL LISTVIEW
+        Try
+            con.Open()
+            adapter = New OleDbDataAdapter(cmd)
+            adapter.Fill(dt)
+
+            'LOOP THRU DATATABLE
+            For Each row In dt.Rows
+                Populate(row(0), row(1), row(2), row(3))
+            Next
+
+            'CLEAR DATATABLE
+            dt.Rows.Clear()
+            con.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            con.Close()
+        End Try
+    End Sub
+
+    'UPDATE DATABASE
+    Private Sub UpdateLV(id As String, spacecraft As String, propellant As String, destination As String)
+        'SQL STATEMENT
+        Dim sql As String = "UPDATE spacecraftsTB SET S_Name='" + spacecraft + "',S_Propellant='" + propellant + "',S_Destination='" + destination + "' WHERE ID=" + id + ""
+        cmd = New OleDbCommand(sql, con)
+
+        'OPEN CONNECTION,EXECUTE UPDATE,CLOSE CONNECTION'
+        Try
+            con.Open()
+            adapter = New OleDbDataAdapter(cmd)
+            adapter.UpdateCommand = con.CreateCommand()
+            adapter.UpdateCommand.CommandText = sql
+
+            If (adapter.UpdateCommand.ExecuteNonQuery() > 0) Then
+                MsgBox("Successfully Updated")
+                CleartextBoxes()
+            End If
+            con.Close()
+            Retrieve()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            con.Close()
+        End Try
+    End Sub
+
+    'DELETE FROM DATABASE
+    Private Sub Delete(id As String)
+        'ELETE SQL STATEMENT
+        Dim sql As String = "DELETE FROM spacecraftsTB WHERE ID=" + id + ""
+        cmd = New OleDbCommand(sql, con)
+
+        'OPEN CONNECTION,EXECUTE DELETE,CLOSE CONNECTION
+        Try
+            con.Open()
+            adapter = New OleDbDataAdapter(cmd)
+            adapter.DeleteCommand = con.CreateCommand()
+            adapter.DeleteCommand.CommandText = sql
+
+            'PROMPT FOR CONFIRMATION BEFORE DELETE
+            If MessageBox.Show("Are you sure to permanently delete this?", "DELETE", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.OK Then
+                If cmd.ExecuteNonQuery() > 0 Then
+                    CleartextBoxes()
+                    MsgBox("Successfully deleted")
+                End If
+            End If
+            con.Close()
+            Retrieve()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            con.Close()
+        End Try
+    End Sub
+
+    'RESET TEXTBOXES
+    Private Sub CleartextBoxes()
+        nameTxt.Text = ""
+        propellantTxt.Text = ""
+        destinationTxt.Text = ""
+    End Sub
+
+    'ADD BUTTON CLICKED
+    Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
+        Add()
+    End Sub
+
+    'RETRIEVE BUTTON CLICKED
+    Private Sub retrieveBtn_Click(sender As Object, e As EventArgs) Handles retrieveBtn.Click
+        Retrieve()
+    End Sub
+
+    'UPDATE BUTTON CLICKED
+    Private Sub updateBtn_Click(sender As Object, e As EventArgs) Handles updateBtn.Click
+        Dim selectedIndex As Int32 = mListView.SelectedIndices(0)
+        
+        If Not selectedIndex = -1 Then
+            Dim id As String = mListView.SelectedItems(0).SubItems(0).Text
+            ' Dim id As Int32 = Convert.ToInt32(selected)
+            UpdateLV(id, nameTxt.Text, propellantTxt.Text, destinationTxt.Text)
+        End If
+       
+    End Sub
+
+
+    'DELETE BUTTON CLICKED
+    Private Sub deleteBtn_Click(sender As Object, e As EventArgs) Handles deleteBtn.Click
+      Dim selectedIndex As Int32 = mListView.SelectedIndices(0)
+
+        If Not selectedIndex = -1 Then
+            Dim id As String = mListView.SelectedItems(0).SubItems(0).Text
+            ' Dim id As Int32 = Convert.ToInt32(selected)
+            Delete(id)
+        End If
+    End Sub
+
+    'CLEAR BUTTON CLICKED
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        mListView.Items.Clear()
+        CleartextBoxes()
+    End Sub
+
+    'LISTVIEW SELECTION CHANGED
+    Private Sub mListView_SelectedIndexChanged(sender As Object, e As EventArgs) Handles mListView.SelectedIndexChanged
+        Try
+            Dim selectedIndex As Int32 = mListView.SelectedIndices(0)
+
+            If Not selectedIndex = -1 Then
+
+                If mListView.SelectedItems(0).SubItems(0).Text IsNot Nothing Then
+
+                    'GET SELECTED ITEMS
+                    Dim spacecraft As String = mListView.SelectedItems(0).SubItems(1).Text
+                    Dim propellant As String = mListView.SelectedItems(0).SubItems(2).Text
+                    Dim destination As String = mListView.SelectedItems(0).SubItems(3).Text
+
+                    'SET THEM TO TEXTBOXES
+                    nameTxt.Text = spacecraft
+                    propellantTxt.Text = propellant
+                    destinationTxt.Text = destination
+
+
+                End If
+            End If
+
+        Catch ex As ArgumentOutOfRangeException
+
+        End Try
+        
+    End Sub
+
+    
+End Class
